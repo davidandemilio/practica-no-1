11b73ec196d38effd2c048946e2db391921ad8a2
josuedavidgodinez <godinezjosue@hotmail.com>
2018-02-06 17:05:30 -0600

    funcionalidades principales
    pasos iniciales practica

---
+++ ejemplolab1/TDA/ListaOriginal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDA
+{
+    public class ListaOriginal<T>
+    {
+        List<T> lista=new List<T>();
+
+        public void Insertar(T elemento)
+        {
+
+            lista.Add(elemento);
+        }
+        public List<T> obtenerListado()
+        {
+
+            return lista;
+
+        }
+    }
+}
