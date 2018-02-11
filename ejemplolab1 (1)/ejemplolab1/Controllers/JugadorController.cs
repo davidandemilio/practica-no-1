60b5fe6492740cc75e7d068bf132487188f3b6fc
josuedavidgodinez <godinezjosue@hotmail.com>
2018-02-11 15:18:57 -0600

    edicion

---
+++ ejemplolab1 (1)/ejemplolab1/Controllers/JugadorController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ejemplolab1.DBContest;
+using ejemplolab1.Models;
+namespace ejemplolab1.Controllers
+{
+    public class JugadorController : Controller
+    {
+        DefaultConnection db =  DefaultConnection.getInstance;
+        // GET: Jugador
+        public ActionResult Index()
+        {
+     
+            return View(db.Jugadores.obtenerListado());
+        }
+
+        // GET: Jugador/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Jugador/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Jugador/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include="jugadorid,nombre,apellido,salaro,posiscion")]Jugador jugador)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                db.Jugadores.Insertar(jugador);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Jugador/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Jugador/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Jugador/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Jugador/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ejemplolab1 (1)/ejemplolab1/Models/Jugador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ejemplolab1.Models
+{
+    public class Jugador
+    {
+        public int jugadorid{get;set;}
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public int salario { get; set; }
+        public string posiscion { get; set; }
+    }
+}
